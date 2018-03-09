c7c6f06cb78a00e9364b28764ce3d03bbcb08871
amir <amir.harati@jibo.com>
2018-03-08 21:22:53 -0800

    add tokenizer and word/char to id

--- data_prep.py
+++ data_prep.py
@@ -2,10 +2,18 @@
 extract and normalize tweets from raw data and save the resutls.
 
 Amir Harati, March 2018
+issues:
+  1- chunking is not used (e.g. new york would be new + york)
 """
 import json
 import textacy as tc
 import glob, os
+import spacy as sp
+
+outdir = "./data"
+nlp = sp.load('en_core_web_lg')
+
+#nlp = sp.load('en')
 
 json_data = dict()
 os.chdir("./raw_data")
@@ -13,16 +21,88 @@ for file in glob.glob("master*.json"):
   with open(file) as data_file:
     json_data[file] = json.load(data_file)
 
+os.chdir("..")
+
 # read and normalize the tweets
 data = list()
 for k in json_data:
   for d in json_data[k]:
     try:
-      data.append(tc.preprocess.preprocess_text(d["full_text"],
-       transliterate=True, no_urls=True, no_emails=True))
+      temp = tc.preprocess.preprocess_text(tc.preprocess.normalize_whitespace(d["full_text"]), transliterate=True, no_urls=True, no_emails=True)
     except:
-      data.append(tc.preprocess.preprocess_text(d["text"],
-       transliterate=True, no_urls=True, no_emails=True))
+      temp = tc.preprocess.preprocess_text(tc.preprocess.normalize_whitespace(d["text"]), transliterate=True, no_urls=True, no_emails=True)
+    temp = temp.strip("\r\n")
+    temp = temp.replace("\n", " ")
+    temp = temp.replace("\t", " ")
+    data.append(temp)
+
+
+words = set()
+chars = set()
+count = 0
+tweet_to_tokens = {}
+for tweet in data:
+  print(count)
+  count += 1
+  doc = nlp(tweet)
+  tweet_to_tokens[tweet] = doc
+  le = [x for x in tweet]
+  for c in le:
+    chars.add(c)
+  for token in doc:
+    words.add(token.text)
+
+chars = list(chars)
+words = list(words)
+
+print("#chars: ", len(chars))
+print("#words:", len(words))
+
+
+words_to_ids = {w: id + 1 for id, w in enumerate(sorted(words))}
+ids_to_words = {words_to_ids[x]: x for x in words_to_ids}
+chars_to_ids = {w: id + 1 for id, w in enumerate(sorted(chars))}
+ids_to_chars = {chars_to_ids[x]: x for x in chars_to_ids}
+
+# save data
+with open(outdir + "/word2id.txt", "w") as wif:
+  for key, val in words_to_ids.items():
+    wif.write(key + "\t" + str(val) + "\n")
+
+with open(outdir + "/chars2id.txt", "w") as wif:
+  for key, val in chars_to_ids.items():
+    wif.write(key + "\t" + str(val) + "\n")
+
+with open(outdir + "/text_data.txt", "w") as f:
+  for tweet in data:
+    f.write(tweet + "\n")
+
+with open(outdir + "/charid_data.txt", "w") as f:
+  for tweet in data:
+    ostr = ""
+    #le = [x for x in tweet]
+    for c in tweet:
+      ostr = ostr + str(chars_to_ids[c]) + " "
+    f.write(ostr + "\n")
+
+
+with open(outdir + "/wordid_data.txt", "w") as f:
+  for tweet in data:
+    ostr = ""
+    for token in tweet_to_tokens[tweet]:
+      #print(word)
+      word = token.text
+      ostr = ostr + str(words_to_ids[word]) + " "
+    f.write(ostr + "\n")
+
+
+
+
+
+# outputs: 1 raw tweets, 2 tweets in integer format
+# 3 list of non-stop words (also entety recogion?) in both raw and integer for each tweet
+# Do we need further normalization like remove @xxx ?
+#
 
 # next tokenize for word based LM (and do other needed things)
 # anohter version for char based
