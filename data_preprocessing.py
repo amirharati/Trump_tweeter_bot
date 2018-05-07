e95a448ac999f7dfe0a9b817b4a636e7feea1092
amir <amir.harati@gmail.com>
2018-05-06 20:54:33 -0700

    update data_preprocessing and add speeches to data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,9 +9,10 @@ import json
 import textacy as tc
 import glob, os
 import spacy as sp
+import unicodedata
 
 outdir = "./data"
-nlp = sp.load('en_core_web_lg')
+#nlp = sp.load('en_core_web_lg')
 
 #nlp = sp.load('en')
 
@@ -32,6 +33,7 @@ for k in json_data:
     except:
       temp = d["text"]
 
+    temp = str(unicodedata.normalize('NFKD', temp).encode('ascii', 'ignore'))
     temp = temp.strip("\r\n")
     temp = temp.replace("\n", " ")
     temp = temp.replace("\t", " ")
@@ -40,14 +42,16 @@ for k in json_data:
     temp = temp.replace("*URL*", "")
     temp = temp.replace("&amp", "")
     temp = temp.replace("&lt", "")
+    temp = temp.replace("\"", "")
+    temp = temp.replace("\'", "")
     temp = temp.replace("&gt", "")
     temp = temp.replace("-", "")
-    temp = temp.replace(".", "")
+    #temp = temp.replace(".", "")
     temp = temp.replace("    ", " ")
     temp = temp.replace("   ", " ")
     temp = temp.replace("  ", "")
-
-
+    temp = temp.replace('?', ' ? ')
+    temp = temp.replace('!', ' ! ')
 
     data.append(temp)
 
@@ -61,29 +65,75 @@ chars = set()
 #chars.add("<EOS>")
 #  For now it  is  not acally used.
 
-count = 0
-tweet_to_tokens = {}
-for tweet in data:
-  print(count)
-  count += 1
-  doc = nlp(tweet)
-  tweet_to_tokens[tweet] = doc
-  le = [x for x in tweet]
-  for c in le:
-    chars.add(c)
-  for token in doc:
-    words.add(token.text)
+#count = 0
+#tweet_to_tokens = {}
+#for tweet in data:
+#  print(count)
+#  count += 1
+#  doc = nlp(tweet)
+#  tweet_to_tokens[tweet] = doc
+#  le = [x for x in tweet]
+#  for c in le:
+#    chars.add(c)
+#  for token in doc:
+#    words.add(token.text)
+
+#data = [item for sublist in data for item in sublist]
+
+#print(data[0:1000])
+
+speeches = [line.strip() for line in open("data/trump_speeches.txt")]
+sdata = []
+for line in speeches:
+  temp = line.strip("\r\n")
+  temp = temp.replace("\n", " ")
+  temp = temp.replace("\t", " ")
+  temp = tc.preprocess.normalize_whitespace(tc.preprocess.preprocess_text(temp, transliterate=True, no_urls=True, no_emails=True))
+  temp = temp.replace("*EMAIL*", "")
+  temp = temp.replace("*URL*", "")
+  temp = temp.replace("&amp", "")
+  temp = temp.replace("&lt", "")
+  temp = temp.replace("\"", "")
+  temp = temp.replace("\'", "")
+  temp = temp.replace("&gt", "")
+  temp = temp.replace("-", "")
+  #temp = temp.replace(".", "")
+  temp = temp.replace("    ", " ")
+  temp = temp.replace("   ", " ")
+  temp = temp.replace("  ", "")
+  temp = temp.replace('?', ' ? ')
+  temp = temp.replace('!', ' ! ')
+
+  sdata.append(temp)
+
+# add speeches to  tweets
+data = data + sdata
+
+line = ""
+pdata = []
+words = set()
+chars = set()
+for l in data:
+  for w in l.split():
+    words.add(w)
+    for c in w:
+      chars.add(c)
+    #line += w + " "
+  #else:
+  pdata.append(l)
+  #line = ""
+
 
 chars = list(chars)
 words = list(words)
 
-print("#chars: ", len(chars))
-print("#words:", len(words))
-
 words = sorted(words)
 chars = sorted(chars)
 words = ["<PAD>", "<START>", "<EOS>"] + words
-chars = ["<PAD>", "<START>", "<EOS>"] + chars
+chars = ["<PAD>", "<START>", "<EOS>", " "] + chars
+
+print("#chars: ", len(chars))
+print("#words:", len(words))
 
 words_to_ids = {w: id for id, w in enumerate(words)}
 ids_to_words = {words_to_ids[x]: x for x in words_to_ids}
@@ -101,11 +151,11 @@ with open(outdir + "/chars2id.txt", "w") as wif:
     wif.write(key + "\t" + str(val) + "\n")
 
 with open(outdir + "/text_data.txt", "w") as f:
-  for tweet in data:
+  for tweet in pdata:
     f.write(tweet + "\n")
 
 with open(outdir + "/charid_data.txt", "w") as f:
-  for tweet in data:
+  for tweet in pdata:
     ostr = ""
     #le = [x for x in tweet]
     for c in tweet:
@@ -114,13 +164,21 @@ with open(outdir + "/charid_data.txt", "w") as f:
 
 
 with open(outdir + "/wordid_data.txt", "w") as f:
-  for tweet in data:
+  for sen in pdata:
     ostr = ""
-    for token in tweet_to_tokens[tweet]:
+    for word in sen.split():
       #print(word)
-      word = token.text
       ostr = ostr + str(words_to_ids[word]) + " "
     f.write(ostr + "\n")
+
+# with open(outdir + "/wordid_data.txt", "w") as f:
+#   for tweet in pdata:
+#     ostr = ""
+#     for token in tweet_to_tokens[tweet]:
+#       #print(word)
+#       word = token.text
+#       ostr = ostr + str(words_to_ids[word]) + " "
+#     f.write(ostr + "\n")
 
 
 # another script for dividing to train/test sets
