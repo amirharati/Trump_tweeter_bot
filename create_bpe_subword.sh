8fd9098ddf235a5c02886c9d9132a007e3dedea4
amir <amir.harati@gmail.com>
2018-08-10 20:11:14 -0700

    update creaye bpe and generated files

--- create_bpe_subword.sh
+++ create_bpe_subword.sh
@@ -6,18 +6,26 @@
 # uncomment if not cloned.
 #git clone https://github.com/rsennrich/subword-nmt.git
 
-subword-nmt/learn_joint_bpe_and_vocab.py --input data/questions.txt data/answers.txt -s 50000 -o data/trump_code.bpe --write-vocabulary data/vocab_questions.bpe data/vocab_answers.bpe
+# put them together so we can share the vocab. btween encoder/decoder.
+cat data/questions.txt data/answers.txt > data/questions_answers.txt
+
+subword-nmt/learn_joint_bpe_and_vocab.py --input data/questions_answers.txt -s 50000 -o data/trump_code.bpe --write-vocabulary data/vocab_questions_answers.bpe
 # remove tab  (what about some other chars)
-sed -i '/\t/d' data/vocab_questions.bpe 
-sed -i '/\t/d' data/vocab_answers.bpe 
-cat data/vocab_questions.bpe | cut -f1 --delimiter=' ' > data/revocab_questions.bpe
-cat data/vocab_answers.bpe | cut -f1 --delimiter=' ' > data/revocab_answers.bpe
+sed -i '/\t/d' data/vocab_questions_answers.bpe 
+#sed -i '/\t/d' data/vocab_answers.bpe 
+#cat data/vocab_questions.bpe | cut -f1 --delimiter=' ' > data/revocab_questions.bpe
+#cat data/vocab_answers.bpe | cut -f1 --delimiter=' ' > data/revocab_answers.bpe
 
-subword-nmt/apply_bpe.py -c data/trump_code.bpe --vocabulary data/vocab_questions.bpe --vocabulary-threshold 10 < data/questions.txt  >  data/questions.bpe
-subword-nmt/apply_bpe.py -c data/trump_code.bpe --vocabulary data/vocab_answers.bpe --vocabulary-threshold 10 < data/answers.txt  >  data/answers.bpe
+# we select at least 500 since we inflate the data 
+subword-nmt/apply_bpe.py -c data/trump_code.bpe --vocabulary data/vocab_questions_answers.bpe --vocabulary-threshold 500 < data/questions.txt  >  data/questions.bpe
+subword-nmt/apply_bpe.py -c data/trump_code.bpe --vocabulary data/vocab_questions_answers.bpe --vocabulary-threshold 500 < data/answers.txt  >  data/answers.bpe
+
+cat data/questions.bpe data/answers.bpe > data/questions_answers.bpe
+
+subword-nmt/get_vocab.py  <data/questions_answers.bpe > data/vocab_selected_questions_answers.bpe
+
+#subword-nmt/get_vocab.py  <data/answers.bpe > data/vocab_selected_answers.bpe
 
 
-subword-nmt/get_vocab.py  <data/questions.bpe > data/vocab_selected_questions.bpe
-subword-nmt/get_vocab.py  <data/answers.bpe > data/vocab_selected_answers.bpe
-
+# TODO: use pretrained model.
 ###  prettrained:  https://github.com/bheinzerling/bpemb
